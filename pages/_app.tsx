f5ae67ffb24f80f2b22d52dc1fb6cd7081c72804
Dagbrain <dagrainion@gmail.com>
2023-01-30 21:19:38 +0000

    description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,11 +11,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
       <Head>
-        <title>thirdweb NFT Drop Minting Customizable Page</title>
+        <title>Dagrainion NFT Drop Minting Page</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
-          content="Learn How To Use Thirdweb's NFT Drop contract and create a customizable NFT Drop minting page"
+          content="Here you can mint a Dagrainion Warrior NFT"
         />
         <meta
           name="keywords"
